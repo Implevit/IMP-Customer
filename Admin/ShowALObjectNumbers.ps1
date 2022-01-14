7d86bad173e3e275018646a6e59069b3016d03ab
RME_IMP <r.meurer@implevit.ch>
2022-01-14 15:13:05 +0100

    Tidied up Admin-Folder

--- Admin/ShowALObjectNumbers.ps1
+++ Admin/ShowALObjectNumbers.ps1
@@ -1,2 +1,1 @@
-\\impfps01\Daten\04_Entwicklung\Kunden\IMP\Admin\Apps\ShowALObjectNumbers.ps1 -paraAppName "imp-basic"
 \\impfps01\Daten\04_Entwicklung\Kunden\IMP\Admin\Apps\ShowALObjectNumbers.ps1 -paraAppName "imp-customer"
